22f66090065b7881c44da836e849a44bd85707d4
Sandeep Singh <sandeepsinghpreet80055@gmail.com>
2024-09-05 08:06:10 +0530

    typeOf and typeCasting added

---
+++ typeCasting.js
@@ -0,0 +1,9 @@
+const str = "1234";
+let num = Number(str);
+console.log(str, "is changed in", num);
+
+const num2 = 456;
+let str2 = String(num2);
+
+console.log(num2, "is changed in", str2);
+console.log(Number("19") + 2);
---
+++ typeOf.js
@@ -0,0 +1,7 @@
+console.log(typeof "hello"); // "string"
+console.dir(typeof 23); // "number"
+console.log(typeof true); // "boolean"
+console.log(typeof undefined);
+
+console.log(typeof typeof typeof typeof typeof true);
+console.log(typeof 10e2); // typeof 1000 = "number"
