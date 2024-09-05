697f34410cea5fb35a5e03b21e159fd00f371c0d
Sandeep Singh <sandeepsinghpreet80055@gmail.com>
2024-09-05 20:47:04 +0530

    morning class code updated

--- typeCasting.js
+++ typeCasting.js
@@ -1,9 +1,13 @@
 const str = "1234";
 let num = Number(str);
 console.log(str, "is changed in", num);
+console.log(typeof num);
+console.log(typeof str);
 
 const num2 = 456;
 let str2 = String(num2);
-
 console.log(num2, "is changed in", str2);
-console.log(Number("19") + 2);
+console.log(typeof num2);
+console.log(typeof str2);
+
+console.log(Number("1") + 2);
