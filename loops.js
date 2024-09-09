f76f739cc7cdfd49816e140078e460d6e231c6a2
Sandeep Singh <sandeepsinghpreet80055@gmail.com>
2024-09-09 21:05:17 +0530

    loops added morning batch

---
+++ loops.js
@@ -0,0 +1,20 @@
+let iteration = 0;
+
+while (iteration <= 10) {
+  console.log(iteration);
+  iteration++;
+}
+
+for (let iteration = 0; iteration <= 10; iteration++) {
+  console.log(iteration);
+}
+
+const str = "hello";
+for (let i = 0; i < str.length; i++) {
+  console.log(str[i]);
+}
+// console.log(str[0]);
+// console.log(str[1]);
+// console.log(str[2]);
+// console.log(str[3]);
+// console.log(str[4]);
