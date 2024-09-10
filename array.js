06443b492da7e108064d34593ce222e0f8ba7c18
Sandeep Singh <sandeepsinghpreet80055@gmail.com>
2024-09-10 21:05:53 +0530

    morning batch

---
+++ array.js
@@ -0,0 +1,31 @@
+let arr = [12, 23, "str", true, [1, 3], null, undefined];
+console.log(arr);
+
+// properties
+console.log(arr.length);
+console.log(arr[0] + 8);
+console.log(arr[1]);
+
+console.log(arr[4][1]);
+
+const array = [];
+// methods
+// 1. push() :- just adds a item to the end.
+array.push("waheguru");
+array.push("waheguru-2");
+console.log(array);
+
+// 2. pop() :- just removes a item from the end, and it's returns that removed value
+const removedValue = array.pop();
+console.log(removedValue);
+console.log(array);
+
+// 3. unshift() :- just adds a item to the front.
+array.unshift(removedValue);
+array.unshift("khalsa");
+console.log(array);
+
+// 4. shift() :- just remove a item from the front, and it's returns that removed value
+const removedValue2 = array.shift();
+console.log(removedValue2);
+console.log(array);
