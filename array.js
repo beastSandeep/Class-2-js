d2eb337bb272073b06329f21f882a8b6505985d5
Sandeep Singh <sandeepsinghpreet80055@gmail.com>
2024-09-11 07:54:28 +0530

    array added

--- array.js
+++ array.js
@@ -2,11 +2,11 @@ let arr = [12, 23, "str", true, [1, 3], null, undefined];
 console.log(arr);
 
 // properties
-console.log(arr.length);
-console.log(arr[0] + 8);
-console.log(arr[1]);
+console.log(arr.length); // 7
+console.log(arr[0] + 8); // 12 + 8
+console.log(arr[1]); // 23
 
-console.log(arr[4][1]);
+console.log(arr[4][1]); // 3
 
 const array = [];
 // methods
@@ -25,7 +25,7 @@ array.unshift(removedValue);
 array.unshift("khalsa");
 console.log(array);
 
-// 4. shift() :- just remove a item from the front, and it's returns that removed value
+// 4. shift() :- just removes a item from the front, and it's returns that removed value
 const removedValue2 = array.shift();
 console.log(removedValue2);
 console.log(array);
