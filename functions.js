d8d4ba19d8df0c342c7c2de0f5a83b7002f6adce
Sandeep Singh <sandeepsinghpreet80055@gmail.com>
2024-09-20 08:22:13 +0530

    functions added

---
+++ functions.js
@@ -0,0 +1,30 @@
+function hello(param1, param2) {
+  // param1, param2 are pareameters
+  console.log(param1);
+  console.log(param2);
+  console.log("Sandeep");
+}
+
+// "Mandeep", "Arsh" are arguments for param1, param2 pareameters
+hello("Mandeep", "Arsh");
+
+// Make a function that takes two numbers and it's job is just printing addition of passed numbers
+function add(num1, num2) {
+  console.log(num1 + num2);
+}
+add(2, 6);
+
+// Make a function that takes one number and it's job is just printing square of passed number
+function square(num) {
+  console.log(num * num);
+}
+square(6);
+
+// Make a function that takes one number and it's job is just printing cube of passed number
+function cube(num) {
+  // console.log(num * num * num);
+  console.log(num ** 3);
+}
+cube(6);
+
+// Make a function that takes two numbers and it's job is just doing exponents of numbers ex. 3⁵.
