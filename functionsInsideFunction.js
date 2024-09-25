f1d928fe7a6b0bee059338afabfc248f246496b6
Sandeep Singh <sandeepsinghpreet80055@gmail.com>
2024-09-25 07:58:42 +0530

    function inside functions

--- functionsInsideFunction.js
+++ functionsInsideFunction.js
@@ -9,13 +9,23 @@ function app() {
 
   const multiply = (num1, num2) => {
     console.log(num1 * num2);
+    return num1 * num2;
   };
 
   console.log("Inside app");
 
-  multiply(3, 5);
+  const fifteen = multiply(3, 5);
+  console.log(fifteen + 5);
   myFunc();
   add(3, 5);
 }
 
 app();
+
+// function hi() {
+//   const message = "hello";
+//   return message;
+// }
+
+// const res = hi();
+// console.log(res);
