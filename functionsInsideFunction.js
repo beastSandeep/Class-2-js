f503ff44f44db111d3b493177efe9ae57e70e2a2
Sandeep Singh <sandeepsinghpreet80055@gmail.com>
2024-09-25 21:22:42 +0530

    lexical scoped added

--- functionsInsideFunction.js
+++ functionsInsideFunction.js
@@ -8,7 +8,6 @@ function app() {
   };
 
   const multiply = (num1, num2) => {
-    console.log(num1 * num2);
     return num1 * num2;
   };
 
---
+++ lexicalScope.js
@@ -0,0 +1,16 @@
+// function a() {
+//   function b() {
+//     const y = "hello";
+//     console.log("inside myFunc", y);
+//   }
+//   b();
+//   console.log(y);
+// }
+// a();
+
+// console.log(y);
+
+if (true) {
+  const x = "X";
+}
+console.log(x);
