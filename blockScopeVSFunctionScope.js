6ec63b58294b4f5e68db53520fe1e3b61298106e
Sandeep Singh <sandeepsinghpreet80055@gmail.com>
2024-09-26 21:12:04 +0530

    block scope and function scope added

---
+++ blockScopeVSFunctionScope.js
@@ -0,0 +1,52 @@
+// 1. Block Scope
+
+// {} is called block (if, loop, else, switch)
+// let and const are block scoped, means they can be accesed in block only(not outside of a block)
+// var is not block scope(bad thing), means this can be accessed outside of a block
+
+// ex:-
+if (true) {
+  const a = 2;
+  let b = 12;
+  var c = 15;
+}
+// console.log(a); // not defined
+// console.log(b);// not defined
+// console.log(c); // 15 (bad thing)
+// const c = 13; // gives you error because c has already been declared inside if-block 😎
+
+// 2. Function Scope
+// var is only accessible inside function.
+
+// ex:-
+function hello() {
+  var greeting = "hello, how are you";
+  console.log(greeting);
+}
+// console.log(greeting); // not defined
+
+// exercise 1:-
+// if (true) {
+//   var firstName = "Sandeep";
+// }
+
+// if (true) {
+//   console.log(firstName); // Sandeep
+// }
+
+// exercise 2:-
+function x() {
+  if (true) {
+    var firstName = "Sandeep";
+    console.log(firstName); // Sandeep
+  }
+
+  var firstName = "Manpreet";
+  if (true) {
+    console.log(firstName); // Manpreet
+  }
+  console.log(firstName); // Manpreet
+}
+
+x();
+console.log(firstName); // error: firstName is not defined
--- lexicalScope.js
+++ lexicalScope.js
@@ -1,11 +1,40 @@
-function a() {
-  function b() {
-    const y = "hello";
-    console.log("inside myFunc", y);
-  }
-  b();
-  console.log(y);
-}
-a();
+// const x = "vlaue 10";
 
-console.log(y);
+// function myApp() {
+//   const x = "vlaue 1";
+
+//   function myFunc() {
+//     const x = "vlaue 59";
+//     console.log("inside myFunc", x);
+//   }
+
+//   function muFunc2() {
+//     const x = "vlaue 35";
+//     console.log("inside myFunc2", x);
+//   }
+
+//   console.log("inside myApp", x);
+//   muFunc2();
+//   myFunc();
+// }
+
+// myApp();
+
+const myVar = "value 1";
+
+function myApp() {
+  function myFunc() {
+    const myVar = "Sandeep";
+
+    const myFunc2 = () => {
+      console.log("inside myFunc2", myVar); // inside myFunc2 Sandeep
+    };
+
+    myFunc2();
+    console.log(myVar); // Sandeep
+  }
+
+  console.log(myVar); // value 1
+  myFunc();
+}
+myApp();
