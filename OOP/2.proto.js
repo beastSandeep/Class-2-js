cc1302837f618a1f6c72b29e78d14635e299e754
Sandeep Singh <sandeepsinghpreet80055@gmail.com>
2024-10-23 08:07:16 +0530

    proto added

---
+++ OOP/2.proto.js
@@ -0,0 +1,20 @@
+// proto is just a pointing(refrence) object for an object
+const obj1 = {
+  key1: "value1",
+  key2: "value2",
+};
+
+// obj2 is empty object
+// we are just creating obj2 with setting proto of it
+// here obj1 is proto for obj2
+// so now what we seach in obj2 it will be seached in obj1
+const obj2 = Object.create(obj1);
+
+// we are adding key2 in obj2
+obj2.key3 = "value3";
+
+// we can see whole obj1 as proto of obj2
+console.log("object2's proto is whole object1", obj2.__proto__);
+
+// this key doesn't exist in obj2 but it is present in obj1(proto of obj2)
+console.log(obj2.key1);
