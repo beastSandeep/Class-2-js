a101b908d59e48919c1faf0d7b9f4f35eef62a58
Sandeep Singh <sandeepsinghpreet80055@gmail.com>
2024-11-12 08:02:05 +0530

    DOM added

---
+++ DOM/2.DOM.js
@@ -0,0 +1,5 @@
+// DOM -> Document Object Model
+// Whole HTML file is stored in a object called "document" and this object also part of window object
+
+console.log(window.document);
+console.dir(document);
