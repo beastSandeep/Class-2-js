bf7a1228e2272384d904562597c87f537531fa97
Sandeep Singh <sandeepsinghpreet80055@gmail.com>
2024-11-28 07:58:31 +0530

    randomcolor added

--- Projects/randomColorGenerator/randomColorGenerator.js
+++ Projects/randomColorGenerator/randomColorGenerator.js
@@ -1,10 +1,12 @@
 const btn = document.getElementById("btn");
 const body = document.querySelector("body");
+const h1 = document.querySelector("h1");
 
 btn.addEventListener("click", function () {
   const r = Math.floor(Math.random() * 255);
   const g = Math.floor(Math.random() * 255);
   const b = Math.floor(Math.random() * 255);
 
+  h1.innerText = `rgb(${r},${g},${b})`;
   body.style.backgroundColor = `rgb(${r},${g},${b})`;
 });
