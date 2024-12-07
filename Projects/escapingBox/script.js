41a67d8a195600b09dba2ab13c334f21e4b425d7
Sandeep Singh <sandeepsinghpreet80055@gmail.com>
2024-12-07 08:04:00 +0530

    escaping box bg color added

--- Projects/escapingBox/script.js
+++ Projects/escapingBox/script.js
@@ -6,4 +6,8 @@ box.addEventListener("mouseenter", function () {
 
   box.style.left = `${x}px`;
   box.style.top = `${y}px`;
+
+  box.style.backgroundColor = `rgb(${Math.floor(
+    Math.random() * 256
+  )},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`;
 });
