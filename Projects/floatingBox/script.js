6c568b6da215922f6489f89afea60fb267d48b5e
Sandeep Singh <sandeepsinghpreet80055@gmail.com>
2024-12-07 07:41:30 +0530

    floating box added

--- Projects/floatingBox/script.js
+++ Projects/floatingBox/script.js
@@ -1,7 +1,7 @@
 const box = document.getElementById("box");
 
-let posX = 100;
-let posY = 100;
+let posX = 0;
+let posY = 0;
 
 window.addEventListener("keydown", (e) => {
   const key = String(e.key).toLowerCase();
@@ -9,6 +9,15 @@ window.addEventListener("keydown", (e) => {
   if (key === "w") {
     posY -= 5;
     box.style.top = `${posY}px`;
+  }
+
+  if (key === "a") {
+    posX -= 5;
+    box.style.left = `${posX}px`;
+  }
+  if (key === "d") {
+    posX += 5;
+    box.style.left = `${posX}px`;
   }
 
   if (key === "s") {
