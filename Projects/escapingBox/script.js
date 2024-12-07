3cf16a8bb5ffc416c240436eb182b202b70e24ca
Sandeep Singh <sandeepsinghpreet80055@gmail.com>
2024-12-07 07:58:00 +0530

    escaping box added

---
+++ Projects/escapingBox/script.js
@@ -0,0 +1,9 @@
+const box = document.getElementById("box");
+
+box.addEventListener("mouseenter", function () {
+  const x = Math.round(Math.random() * (window.innerWidth - 50));
+  const y = Math.round(Math.random() * (window.innerHeight - 50));
+
+  box.style.left = `${x}px`;
+  box.style.top = `${y}px`;
+});
