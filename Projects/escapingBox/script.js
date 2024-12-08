b70e3f0e8bbf587d7134beb7a919e037fdddbbc4
Sandeep Singh <sandeepsinghpreet80055@gmail.com>
2024-12-08 06:48:43 +0530

    Advance escaping box added

--- Projects/escapingBox/script.js
+++ Projects/escapingBox/script.js
@@ -1,13 +1,30 @@
-const box = document.getElementById("box");
+const boxs = document.getElementsByClassName("box");
 
-box.addEventListener("mouseenter", function () {
-  const x = Math.round(Math.random() * (window.innerWidth - 50));
-  const y = Math.round(Math.random() * (window.innerHeight - 50));
+function move(box) {
+  box.addEventListener("mouseenter", () => {
+    const x = Math.round(Math.random() * (window.innerWidth - 50));
+    const y = Math.round(Math.random() * (window.innerHeight - 50));
 
-  box.style.left = `${x}px`;
-  box.style.top = `${y}px`;
+    box.style.left = `${x}px`;
+    box.style.top = `${y}px`;
 
-  box.style.backgroundColor = `rgb(${Math.floor(
-    Math.random() * 256
-  )},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`;
-});
+    box.style.backgroundColor = `rgb(${Math.floor(
+      Math.random() * 256
+    )},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`;
+  });
+  box.addEventListener("click", () => {
+    const x = Math.round(Math.random() * (window.innerWidth - 50));
+    const y = Math.round(Math.random() * (window.innerHeight - 50));
+
+    box.style.left = `${x}px`;
+    box.style.top = `${y}px`;
+
+    box.style.backgroundColor = `rgb(${Math.floor(
+      Math.random() * 256
+    )},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`;
+  });
+}
+
+for (let i = 0; i < boxs.length; i++) {
+  move(boxs[i]);
+}
