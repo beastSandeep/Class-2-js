d3a84f7c7691e6c2509506ea0a6fc94b0367f574
Sandeep Singh <sandeepsinghpreet80055@gmail.com>
2024-12-31 09:01:04 +0530

    radius changeing in ball radius

--- Projects/ballBounce/index.js
+++ Projects/ballBounce/index.js
@@ -4,7 +4,8 @@ canvas.height = window.innerHeight - 5;
 
 const ctx = canvas.getContext("2d");
 
-const speed = 5;
+const speed = 50;
+let raf;
 
 const ball = {
   x: Math.floor(window.innerWidth / 2),
@@ -38,16 +39,20 @@ function draw() {
   ball.y += ball.vy;
 
   if (ball.x + ball.vx > boundry.maxX || ball.x + ball.vx < boundry.minX) {
-    ball.vx = -ball.vx;
-    // color chage
-    ball.hue += Math.random() * 10;
+    ball.vx = -ball.vx; // change direction
+    ball.hue += Math.random() * 10; // chage color
+    ball.radius += 2; // change radius
   }
   if (ball.y + ball.vy > boundry.maxY || ball.y + ball.vy < boundry.minY) {
-    ball.vy = -ball.vy;
-    // color chage
-    ball.hue += Math.random() * 10;
+    ball.vy = -ball.vy; // change direction
+    ball.hue += Math.random() * 10; // chage color
+    ball.radius += 2; // change radius
   }
 
-  requestAnimationFrame(draw);
+  raf = requestAnimationFrame(draw);
+
+  if (ball.radius > boundry.maxX || ball.radius > boundry.maxY) {
+    cancelAnimationFrame(raf);
+  }
 }
-requestAnimationFrame(draw);
+raf = requestAnimationFrame(draw);
